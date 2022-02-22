ad2c034ac457511baa38abeb5fcd9750cdc1cd54
mehmetby <100214170+mehmetby@users.noreply.github.com>
2022-02-22 17:25:27 +0300

    A

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+string str1 = string.Empty;
+str1 = "Mehmet BAYSOY";
+string ad = "Mehmet";
+string soyad = "BAYSOY";
+string ad_soyad = ad + " " + soyad ;
+Console.WriteLine(ad_soyad);
