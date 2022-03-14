ff69596d81aa5c15ec736050ecd39f53cec647e2
mehmetby <100214170+mehmetby@users.noreply.github.com>
2022-03-14 19:50:37 +0300

    Switch-Case Yapısı
    Switch-Case Yapısı ile ilgili ders örnekleri.

--- Program.cs
+++ Program.cs
@@ -4,29 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IfElse
+namespace SwitchCaseKararYapilari
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int time = DateTime.Now.Hour;
-            string sonuc;
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız.");
+                    break;
 
-            if (time > 5 && time < 11)
-                sonuc = "Günaydın!";
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız.");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız.");
 
-            else if (time > 11 && time < 18)
-                sonuc = "İyi Günler!";
+                    break;
 
-            else if (time > 18 && time < 22)
-                sonuc = "İyi Akşamlar!";
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız.");
+                    break;
+            }
 
-            else
-                sonuc = "İyi Geceler!";
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Ayındasınız.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Ayındasınız.");
+                    Console.ReadLine();
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Ayındasınız.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Ayındasınız.");
+                    break;
 
-            Console.WriteLine(sonuc);
-            Console.ReadLine();
+
+                default: Console.WriteLine("Değişken Tanımlanamadı!!");
+                  break;
+
+
+            }
+
 
 
         }
