0e1b86ac699c9c2a1a44f6cd8d0aa77c1c0efdff
mehmetby <100214170+mehmetby@users.noreply.github.com>
2022-03-20 15:33:58 +0300

    Array Sınıfı ve Methodlari
    Array Sınıfı ve Methodlari ile ilgili uygulama.

--- Program.cs
+++ Program.cs
@@ -4,51 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Diziler
+namespace ArrayMethodlari
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //  Dizi Tanımlama
-            string[] renkler = new string[5];
-            string[] hayvanlar = { "Flamingo", "At", "Jaguar", "Puma" };
-            int[] dizi;
-            dizi = new int[5];
+            int[] SayiDizisi = { 3, 13, 5, 11, 7, 9, 17, 15 };
+            Console.WriteLine("******Sırasız Dİzi******");
+            foreach (int sayi in SayiDizisi)
+                Console.WriteLine(sayi);
 
-            // Değer Atama ve Erişim
+            Console.WriteLine("******Array Sort******");
+            Array.Sort(SayiDizisi);
+            foreach (int sayi in SayiDizisi)
+                Console.WriteLine(sayi);
 
-            dizi[0] = 10;
-            renkler[3] = "Mavi";
+            Console.WriteLine("******Array Clear******");
+            Array.Clear(SayiDizisi, 2, 2);
+            foreach (int sayi in SayiDizisi)
+                Console.WriteLine(sayi);
 
-            Console.WriteLine(dizi[0]);
-            Console.WriteLine(renkler[3]);
-            Console.WriteLine(hayvanlar[0]);
+            Console.WriteLine("******Array IndexOf******");
+            Console.WriteLine(Array.IndexOf(SayiDizisi, 17));
 
+            Console.WriteLine("******Array Reverse******");
+            Array.Reverse(SayiDizisi);
+            foreach (int sayi in SayiDizisi)
+                Console.WriteLine(sayi);
 
-
-            // Döngülerle Dizi Kullanımı
-            //Klavyeden girilen n tane sayının ortalamasını almak.
-
-            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
-            int ElemanSayisi = int.Parse(Console.ReadLine());
-            int[] SayiDizisi = new int[ElemanSayisi];
-
-            for (int i = 0; i < ElemanSayisi; i++)
-            {
-                Console.Write("Lütfen dizinin {0}. elemanını giriniz: ", i + 1);
-                SayiDizisi[i] = int.Parse(Console.ReadLine());
-            }
-
-            int toplam = 0;
-
-            foreach (var sayi in SayiDizisi)
-                toplam += sayi;
+            Console.WriteLine("******Array ReSize******");
+            Array.Resize<int>(ref SayiDizisi, 9);
+            SayiDizisi[8] = 99;
+            foreach (int sayi in SayiDizisi)
+                Console.WriteLine(sayi);
 
 
-            Console.WriteLine("Ortalama : " + toplam / ElemanSayisi);
+            Console.ReadLine();
 
         }
-
     }
 }
