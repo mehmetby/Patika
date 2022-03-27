dabc978d48e539072b36be87493e9f522360b82d
mehmetby <100214170+mehmetby@users.noreply.github.com>
2022-03-27 15:55:08 +0300

    Metot Overloading ve Out Parametre Kullanımı
    Metot Overloading ve Out Parametre Kullanımı
    https://app.patika.dev/courses/csharp-101/2-metot-overload-nedir-ve-out-kullanimi

--- Program.cs
+++ Program.cs
@@ -4,51 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Methodlar
+namespace MethodOverloading
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = 6;
-            int b = 3;
-            Console.WriteLine(a + b);
-
-            int sonuc = Topla(a, b);
-            Console.WriteLine(sonuc);
-
-
-            methods ornek = new methods(); // Class'ta yazdığımız metoda ulaşabilmek için tanımladık.
-            ornek.EkranaYazdir(Convert.ToString(sonuc)); // sonuc int fakat metodumuz string ifade yazdırıyor. o yüzden Convert.ToInt'ı kullanıyoruz.
-
-            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
-            ornek.EkranaYazdir(Convert.ToString(sonuc2));
-            ornek.EkranaYazdir(Convert.ToString(a + b));
-           
-
-            Console.ReadLine();
-
-        }
-        static int Topla(int deger1, int deger2)
-        {
-            return (deger1 + deger2);
-        }
-
-
-    }
-    class methods
-    {
-        public void EkranaYazdir(string veri) // EkranaYazdir metoduna her yerden erişebilmek için public olarak tanımlıyoruz.
-        {
-            Console.WriteLine(veri);
-        }
-
-        public int ArttirVeTopla(ref int deger1, ref int deger2)
-        {
-            deger1 += 1;
-            deger2 += 1;
-            return deger1 + deger2;
+            Console.WriteLine("hello World");
         }
     }
-
 }
