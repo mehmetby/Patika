add9e2ca89efd661660317a702dad6f00fc7c480
mehmetby <100214170+mehmetby@users.noreply.github.com>
2022-03-27 16:00:13 +0300

    Metot Overloading ve Out Parametre Kullanımı
    Metot Overloading ve Out Parametre Kullanımı
    https://app.patika.dev/courses/csharp-101/2-metot-overload-nedir-ve-out-kullanimi

--- Program.cs
+++ Program.cs
@@ -1,16 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MethodOverloading
+namespace MethodsOverLoading
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello World");
+            string Sayi = "999";
+            int OutSayi;
+            bool sonuc = int.TryParse(Sayi, out OutSayi);
+
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı.Veri Tipi Dönüştürülebilir!");
+                Console.WriteLine("OutSayi =" + OutSayi);
+            }
+            else Console.WriteLine("Başarısız.Veri Dönüştürülebilecek Tipte Değil!");
+
+            Methods instance = new Methods();
+            instance.Topla(4, 5, out int ToplamSonucu);
+            Console.WriteLine(ToplamSonucu);
+
+            //Metot OverLoading/Aşırı Yükleme
+            int ifade = 999;
+            instance.EkranaYazdir(ifade);
+
+            //Metot İmzası
+            //Metot Adı+ Parametre Sayısı + Parametre.
+            string Name = "Mehmet";
+            string Surname = "BAYSOY";
+            instance.EkranaYazdir(Name, Surname);
+            //ya da
+            instance.EkranaYazdir("Mehmet1", "BAYSOY");
+        }
+    }
+    class Methods
+    {
+        public void Topla(int a, int b, out int Toplam)
+        {
+            Toplam = a + b;
+        }
+        public void EkranaYazdir(String Veri)
+        {
+            Console.WriteLine(Veri);
+        }
+        public void EkranaYazdir(int Veri)
+        {
+            Console.WriteLine(Veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + " " + veri2);
         }
     }
 }
